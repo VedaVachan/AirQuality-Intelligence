957d56e633a3a0cd50a3cdce317b25efb3568442
Vangala Vedavachan Reddy da24b029 <vachanveda@gmail.com>
2026-06-26 21:21:08 +0530

    Update dashboard.py

--- app/dashboard.py
+++ app/dashboard.py
@@ -86,7 +86,6 @@ def get_live_weather():
 
     API_KEY = "dab3d934bcd3f161840cf38fd88ddbb0"
 
-    city = "Hyderabad"
 
     url = (
         f"https://api.openweathermap.org/data/2.5/weather"
@@ -127,7 +126,7 @@ sample = pd.DataFrame([{
 
 predicted_aqi = float(model.predict(sample)[0])
 
-weather = get_live_weather()
+weather = get_live_weather(selected_city)
 
 # ---------------------------
 # TITLE
@@ -144,9 +143,38 @@ st.markdown("---")
 
 st.sidebar.title("🌍 Air Quality Controls")
 
+cities = [
+    "Ahmedabad",
+    "Bengaluru",
+    "Bhopal",
+    "Bhubaneswar",
+    "Chandigarh",
+    "Chennai",
+    "Coimbatore",
+    "Delhi",
+    "Guwahati",
+    "Hyderabad",
+    "Indore",
+    "Jaipur",
+    "Kanpur",
+    "Kochi",
+    "Kolkata",
+    "Lucknow",
+    "Mumbai",
+    "Nagpur",
+    "Patna",
+    "Pune",
+    "Raipur",
+    "Ranchi",
+    "Surat",
+    "Thiruvananthapuram",
+    "Visakhapatnam"
+]
+
 selected_city = st.sidebar.selectbox(
-    "City",
-    ["Hyderabad"]
+    "Select City",
+    cities,
+    index=cities.index("Hyderabad")
 )
 
 st.sidebar.metric(
@@ -509,16 +537,44 @@ try:
 
     stations = stations[
         stations["city"].str.contains(
-            "Hyderabad",
+            selected_city,
             case=False,
             na=False
         )
     ]
 
-    m = folium.Map(
-        location=[17.3850, 78.4867],
-        zoom_start=10
-    )
+city_coordinates = {
+    "Ahmedabad": [23.0225, 72.5714],
+    "Bengaluru": [12.9716, 77.5946],
+    "Bhopal": [23.2599, 77.4126],
+    "Bhubaneswar": [20.2961, 85.8245],
+    "Chandigarh": [30.7333, 76.7794],
+    "Chennai": [13.0827, 80.2707],
+    "Coimbatore": [11.0168, 76.9558],
+    "Delhi": [28.6139, 77.2090],
+    "Guwahati": [26.1445, 91.7362],
+    "Hyderabad": [17.3850, 78.4867],
+    "Indore": [22.7196, 75.8577],
+    "Jaipur": [26.9124, 75.7873],
+    "Kanpur": [26.4499, 80.3319],
+    "Kochi": [9.9312, 76.2673],
+    "Kolkata": [22.5726, 88.3639],
+    "Lucknow": [26.8467, 80.9462],
+    "Mumbai": [19.0760, 72.8777],
+    "Nagpur": [21.1458, 79.0882],
+    "Patna": [25.5941, 85.1376],
+    "Pune": [18.5204, 73.8567],
+    "Raipur": [21.2514, 81.6296],
+    "Ranchi": [23.3441, 85.3096],
+    "Surat": [21.1702, 72.8311],
+    "Thiruvananthapuram": [8.5241, 76.9366],
+    "Visakhapatnam": [17.6868, 83.2185]
+}
+
+m = folium.Map(
+    location=city_coordinates[selected_city],
+    zoom_start=10
+)
 
     for _, row in stations.iterrows():
 
